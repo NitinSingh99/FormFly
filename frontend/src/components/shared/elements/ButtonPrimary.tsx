407e7116ef3be487c864b48e0a0633751a409bc8
Nitinsingh99 <8761964643ns@gmail.com>
2025-10-20 20:54:28 +0530

    style: text color to btn

--- frontend/src/components/shared/elements/ButtonPrimary.tsx
+++ frontend/src/components/shared/elements/ButtonPrimary.tsx
@@ -5,6 +5,6 @@ export default function SubmitButton({ label, id, className }: SubmitButtonProps
         ? className
         : "w-full";
     return (
-        <button id={id} className={`${className} bg-primary px-4 py-3 rounded-lg hover:bg-primary/90 transition-colors text-lg w-full`}>{label}</button>
+        <button id={id} className={`${className} bg-primary px-4 py-3 rounded-lg hover:bg-primary/90 transition-colors text-lg w-full text-foreground-dark`}>{label}</button>
     );
 }
