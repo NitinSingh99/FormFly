b7b376446a5116ddb0d33ebdb2daa40109be9cfd
Nitinsingh99 <8761964643ns@gmail.com>
2025-10-23 21:14:58 +0530

    style: add some custom style and animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -29,6 +29,23 @@ module.exports = {
         xl: "0.75rem",
         full: "9999px"
       },
+      boxShadow: {
+        'soft': '0 4px 6px -1px rgb(0 0 0 / 0.05), 0 2px 4px -2px rgb(0 0 0 / 0.05)',
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        pulse: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: .5 },
+        }
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'pulse-gentle': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
     },
   },
   plugins: [],
