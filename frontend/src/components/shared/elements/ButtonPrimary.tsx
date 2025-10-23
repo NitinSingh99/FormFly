ae33553b9d38d9e5a4536a7198bcd2abd95ef8f9
Nitinsingh99 <8761964643ns@gmail.com>
2025-10-23 21:32:08 +0530

    refactor: primary btn

--- frontend/src/components/shared/elements/ButtonPrimary.tsx
+++ frontend/src/components/shared/elements/ButtonPrimary.tsx
@@ -1,10 +1,22 @@
 import { SubmitButtonProps } from "../../../types/SubmitButtonProps";
+import Loader from "./Loader";
 
-export default function SubmitButton({ label, id, className, onClick}: SubmitButtonProps) {
-    
+const ButtonPrimary: React.FC<SubmitButtonProps> = ({
+    label,
+    id,
+    className,
+    onClick
+}) => {
     const finalClassName = `${className} bg-primary px-4 py-3 rounded-lg hover:bg-primary/90 transition-colors text-lg w-full text-foreground-dark`;
 
     return (
-        <button id={id} className={finalClassName} onClick={onClick}>{label}</button>
+        <button
+            id={id}
+            onClick={onClick}
+            className={finalClassName}
+        >{label}
+        </button>
     );
-}
+};
+
+export default ButtonPrimary;
