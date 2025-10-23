21f4cf549fa72f9fad1c4936d71b741f45d5e251
Nitinsingh99 <8761964643ns@gmail.com>
2025-10-23 21:34:12 +0530

    ui: add loader on form submit

--- frontend/src/pages/Formpage.tsx
+++ frontend/src/pages/Formpage.tsx
@@ -2,15 +2,28 @@ import IntroSection from "../components/Formpage/sections/IntroSection"
 import UploadBox from "../components/Formpage/sections/UploadBox";
 import FormPreview from "../components/Formpage/sections/FormPreview";
 import AnimatedPage from "../components/shared/elements/AnimatePage";
+import LoadingPage from "./LoadingPage";
+import { useUploadFile } from "../hooks";
 
 export default function FormPage() {
+    const { handleUpload, loading, message, error} = useUploadFile();
     return (
         <>
-            <AnimatedPage>
-                <IntroSection />
-                <UploadBox />
-                <FormPreview />
-            </AnimatedPage>
+            {
+                loading ?
+                    (<>
+                        <AnimatedPage>
+                            <LoadingPage />
+                        </AnimatedPage>
+                    </>) :
+                    (
+                        <AnimatedPage>
+                            <IntroSection />
+                            <UploadBox handleUpload={handleUpload} message={message} error={error}/>
+                            <FormPreview />
+                        </AnimatedPage>
+                    )
+            }
         </>
     );
 }
