21bd184720ca88129f0b41e8d261bd9cdd1e1d62
Nitinsingh99 <8761964643ns@gmail.com>
2025-10-23 21:01:19 +0530

    fix: animatePage

--- frontend/src/components/shared/elements/AnimatePage.tsx
+++ frontend/src/components/shared/elements/AnimatePage.tsx
@@ -4,9 +4,19 @@ const pageVariants = {
   initial: { opacity: 0, y: 20 },
   animate: { opacity: 1, y: 0 },
   exit: { opacity: 0, y: -5 },
-  transition: { duration: 0.2 },
+  transition: { duration: 0.5 },
 };
 
 export default function AnimatedPage({ children }: { children: React.ReactNode }) {
-  return <motion.div {...pageVariants}>{children}</motion.div>;
+  return (
+    <motion.div
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      variants={pageVariants}
+      transition={pageVariants.transition}
+    >
+      {children}
+    </motion.div>
+  );
 }
