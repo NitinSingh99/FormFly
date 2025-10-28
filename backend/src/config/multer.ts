8210b5c5fb0536c684251fbad2fc376b99fcc548
Nitinsingh99 <8761964643ns@gmail.com>
2025-10-28 17:44:40 +0530

    update: pdf storage

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -1,24 +1,5 @@
-import { fileURLToPath } from "url";
-import path from "path";
-import fs from "fs";
 import multer from "multer";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Configure multer storage
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const uploadDir = path.join(__dirname, "../uploads");
-        if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        // Save file with original name + timestamp
-        const ext = path.extname(file.originalname);
-        const baseName = path.basename(file.originalname, ext);
-        cb(null, `${baseName}-${Date.now()}${ext}`);
-    },
-});
+const storage = multer.memoryStorage();
 
 export const upload = multer({ storage });;
