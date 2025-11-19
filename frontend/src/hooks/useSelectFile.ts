e861817a0ba819db1ad8e64e6ba4e7b3f6beab3b
Nitinsingh99 <8761964643ns@gmail.com>
2025-11-19 21:48:47 +0530

    update

--- frontend/src/hooks/useSelectFile.ts
+++ frontend/src/hooks/useSelectFile.ts
@@ -1,12 +1,12 @@
 import { useRef, useState } from "react";
+import { getPdfPageCount } from "../utils/countPage";
 
 export function useSelectFile(allowedType = "application/pdf") {
     const [file, setFile] = useState<File | null>(null);
     const inputRef = useRef<HTMLInputElement>(null);
 
-    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
         const selectedFile = e.target.files?.[0];
-
         if (!selectedFile) return;
 
         if (selectedFile.type !== allowedType) {
@@ -14,17 +14,27 @@ export function useSelectFile(allowedType = "application/pdf") {
             return;
         }
 
+        const pageCount = await getPdfPageCount(selectedFile);
+        if (pageCount > 3) {
+            alert('Number of pages cannot be more than 3');
+            return;
+        }
+
         setFile(selectedFile);
     };
 
-    const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    const handleDrop = async (e: React.DragEvent<HTMLDivElement>) => {
         e.preventDefault();
         const droppedFile = e.dataTransfer.files[0];
-
         if (!droppedFile) return;
-
         if (droppedFile.type !== allowedType) {
             alert("Only PDF files are not allowed!");
+            return;
+        }
+
+        const pageCount = await getPdfPageCount(droppedFile);
+        if (pageCount > 3) {
+            alert('Number of pages cannot be more than 3');
             return;
         }
 
@@ -35,5 +45,5 @@ export function useSelectFile(allowedType = "application/pdf") {
         e.preventDefault();
     }
 
-    return {file, setFile, inputRef, handleFileChange, handleDragOver, handleDrop};
+    return { file, setFile, inputRef, handleFileChange, handleDragOver, handleDrop };
 }
